88c31cf60e45f0dc057bbd1e67f53fd2e8c765d5
Shaikh Saif Jahid Ahemad <shaikhsaifjahidahemad@Shaikhs-MacBook-Pro.local>
2025-09-28 19:49:30 +0530

    deleting repo package

--- Order-Service/src/main/java/com/example/Order_Service/repository/OrderRepo.java
+++
@@ -1,7 +0,0 @@
-//package com.example.Order_Service.repository;
-//
-//import com.example.Order_Service.entity.Orders;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface OrderRepo  extends JpaRepository<Orders,Long> {
-//}
